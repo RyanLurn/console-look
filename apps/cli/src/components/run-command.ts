b4e05caf29289be27d091390eb49d75d6fe6fb8a
Ryan Luong <lumintriet@gmail.com>
2026-01-28 18:58:44 +0700

    feat(cli): handle keyboard interrupt from the parent process.

--- apps/cli/src/components/run-command.ts
+++ apps/cli/src/components/run-command.ts
@@ -1,7 +1,7 @@
 import { readStream } from "#utils/read-stream";
 
 export async function runCommand(command: string[]) {
-  const proccess = Bun.spawn({
+  const subprocess = Bun.spawn({
     cmd: command,
     cwd: process.cwd(),
     env: process.env,
@@ -10,15 +10,23 @@ export async function runCommand(command: string[]) {
     stderr: "pipe",
   });
 
-  readStream(proccess.stdout, (chunk) => {
+  process.on("SIGINT", () => {
+    subprocess.kill("SIGINT");
+  });
+
+  process.on("SIGTERM", () => {
+    subprocess.kill("SIGTERM");
+  });
+
+  readStream(subprocess.stdout, (chunk) => {
     process.stdout.write(chunk);
   });
 
-  readStream(proccess.stderr, (chunk) => {
+  readStream(subprocess.stderr, (chunk) => {
     process.stderr.write(chunk);
   });
 
-  const exitCode = await proccess.exited;
+  const exitCode = await subprocess.exited;
 
   process.exit(exitCode);
 }
