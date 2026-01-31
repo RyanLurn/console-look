11801876e3b0adb01142134270662145620bbc06
Ryan Luong <lumintriet@gmail.com>
2026-01-31 08:47:40 +0700

    feat(api): add database schema.

--- apps/api/src/utils/schemas.ts
+++ apps/api/src/utils/schemas.ts
@@ -3,15 +3,14 @@ import * as z from "zod";
 
 const MAX_TITLE_LENGTH = 120;
 
-export type ChunkId = Branded<string, "ChunkId">;
+export const ChunkIdSchema = z.custom<Branded<string, "ChunkId">>((value) => {
+  const validationResult = z.uuidv7().safeParse(value);
+  if (!validationResult.success) {
+    return false;
+  }
+  return true;
+});
 export const ChunkSchema = z.object({
-  id: z.custom<ChunkId>((value) => {
-    const validationResult = z.uuidv7().safeParse(value);
-    if (!validationResult.success) {
-      return false;
-    }
-    return true;
-  }),
   text: z.string(),
   seq: z.int().positive(), // Stand for "sequence number"
   channel: z.enum(["stdout", "stderr"]),
@@ -19,15 +18,14 @@ export const ChunkSchema = z.object({
 });
 export type Chunk = z.infer<typeof ChunkSchema>;
 
-export type RunId = Branded<string, "RunId">;
+export const RunIdSchema = z.custom<Branded<string, "RunId">>((value) => {
+  const validationResult = z.uuidv7().safeParse(value);
+  if (!validationResult.success) {
+    return false;
+  }
+  return true;
+});
 export const RunSchema = z.object({
-  id: z.custom<RunId>((value) => {
-    const validationResult = z.string().safeParse(value);
-    if (!validationResult.success) {
-      return false;
-    }
-    return true;
-  }),
   title: z
     .string()
     .trim()
@@ -39,7 +37,10 @@ export const RunSchema = z.object({
     }),
   command: z.string().trim().min(1, { error: "Command cannot be empty" }),
   status: z.enum(["running", "finished"]),
-  chunks: z.array(ChunkSchema),
+  chunks: z.record(ChunkIdSchema, ChunkSchema),
   timestamp: z.int().positive(),
 });
 export type Run = z.infer<typeof RunSchema>;
+
+export const DatabaseSchema = z.record(RunIdSchema, RunSchema);
+export type Database = z.infer<typeof DatabaseSchema>;
