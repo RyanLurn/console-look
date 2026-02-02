d95bdd1e54daf6e517572fd31164bacfa0689ca4
Ryan Luong <lumintriet@gmail.com>
2026-02-02 09:39:22 +0700

    feat(web): manually set up shadcn/ui.

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export function App() {
   return (
-    <div className="flex h-dvh flex-col items-center justify-center">
+    <div className="flex h-dvh flex-col items-center justify-center gap-4">
       <h1>Console Look</h1>
+      <Button size="lg">Click me</Button>
     </div>
   );
 }
