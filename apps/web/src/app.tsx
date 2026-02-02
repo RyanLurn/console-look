74989d0d640a425a539760e99e11d8fd15f649f8
Ryan Luong <lumintriet@gmail.com>
2026-02-02 09:23:12 +0700

    chore(root): add Tailwindcss plugin for Prettier.

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,6 +1,6 @@
 export function App() {
   return (
-    <div className="h-dvh flex flex-col items-center justify-center">
+    <div className="flex h-dvh flex-col items-center justify-center">
       <h1>Console Look</h1>
     </div>
   );
