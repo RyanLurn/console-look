123edad67e3125edcddb9e96fc1b1b559ab81cc9
Ryan Luong <lumintriet@gmail.com>
2026-02-02 10:16:33 +0700

    feat(web): configure TanStack Query provider.

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,9 +2,12 @@ import { App } from "@/app";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@/styles.css";
+import { TanstackQueryProvider } from "@/components/providers/tanstack-query";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <TanstackQueryProvider>
+      <App />
+    </TanstackQueryProvider>
   </StrictMode>
 );
