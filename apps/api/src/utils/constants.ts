22db8fb0a9dbe1b079458c8bc4791fe49daa673d
Ryan Luong <lumintriet@gmail.com>
2026-02-03 08:22:46 +0700

    chore(api): remove unused code.

--- apps/api/src/utils/constants.ts
+++ apps/api/src/utils/constants.ts
@@ -1,5 +1,1 @@
-import { join } from "node:path";
-
-export const DATABASE_DIRECTORY = join(import.meta.dir, "..", "database");
-export const STORAGE_DIRECTORY = join(import.meta.dir, "..", "storage");
 export const API_SERVER_PORT = 3000;
